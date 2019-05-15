4cb7a685feb3e83d07c3f70718ad99b06970d76d
Parthghetia <43883264+Parthghetia@users.noreply.github.com>
2019-05-15 13:29:19 -0400

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -4,14 +4,14 @@ var body = document.querySelector('body');
 var input1 = document.getElementById('input1');
 var button = document.getElementById('button');
 var deleteChild = document.getElementById('deleteChild');
-var ul = document.querySelector('ul');
+var ol = document.querySelector('ol');
 function backgroundPicker(){
   body.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
 }
 function createLiElement(){
   var li = document.createElement('li');
   li.appendChild(document.createTextNode(input1.value));
-  ul.appendChild(li);
+  ol.appendChild(li);
   input1.value = "";
 }
 
