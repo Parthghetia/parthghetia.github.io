b290265473d9b52687b7eab37f8b813385a647b5
Parthghetia <43883264+Parthghetia@users.noreply.github.com>
2019-06-14 16:51:22 -0400

    Test
    Test React

---
+++ build/precache-manifest.7dcc3174b057f23774afd9a03ace9b75.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d1fae924d26bc6fef78185ad2fc74f2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9062d81d696bad7b5091",
+    "url": "/static/css/2.e40df323.chunk.css"
+  },
+  {
+    "revision": "6dbd4534af66cb83aa6a",
+    "url": "/static/css/main.ed2237ac.chunk.css"
+  },
+  {
+    "revision": "9062d81d696bad7b5091",
+    "url": "/static/js/2.590933e2.chunk.js"
+  },
+  {
+    "revision": "6dbd4534af66cb83aa6a",
+    "url": "/static/js/main.aa1bc161.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
